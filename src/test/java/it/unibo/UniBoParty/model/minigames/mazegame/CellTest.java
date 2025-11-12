de1aaa641a71a06fbb8b9ca2824d59703cc5eed1
caterinatorelli <caterina.torelli2@studio.unibo.it>
2025-11-12 13:14:08 +0100

    add cell test

---
+++ src/test/java/it/unibo/UniBoParty/model/minigames/mazegame/CellTest.java
@@ -0,0 +1,60 @@
+package it.unibo.UniBoParty.model.minigames.mazegame;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.UniBoParty.model.minigames.mazegame.impl.CellImpl;
+import it.unibo.UniBoParty.utilities.CellType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CellTest {
+    private final int ROW = 5;
+    private final int COL = 10;
+
+    /**
+     * test the initialization and getters of CellImpl.
+     */
+    @Test
+    void testCellPathInitializationAndGetters() {
+        CellImpl cell = new CellImpl(ROW, COL, CellType.EMPTY);
+
+        assertEquals(ROW, cell.getRow(), "row does not match");
+        assertEquals(COL, cell.getCol(), "col does not match");
+        assertEquals(CellType.EMPTY, cell.getType(), "type does not match");
+    }
+
+    /**
+     * test isWalkable method for the CellType.EMPTY type.
+     */
+    @Test
+    void testIsWalkableForPath() {
+        CellImpl cell = new CellImpl(ROW, COL, CellType.EMPTY);
+        assertTrue(cell.isWalkable(), "the EMPTY cells should be walkable");
+    }
+
+    /**
+     * test isWalkable method for the CellType.WALL type.
+     */
+    @Test
+    void testIsWalkableForWall() {
+        CellImpl cell = new CellImpl(ROW, COL, CellType.WALL);
+        assertFalse(cell.isWalkable(), "the WALL cells should not be walkable");
+    }
+
+    /**
+     * test isWalkable method for the CellType.START type.
+     */
+    @Test
+    void testIsWalkableForStart() {
+        CellImpl cell = new CellImpl(ROW, COL, CellType.START);
+        assertTrue(cell.isWalkable(), "the START cells should be walkable");
+    }
+
+    /**
+     * test isWalkable method for the CellType.EXIT type.
+     */
+    @Test
+    void testIsWalkableForExit() {
+        CellImpl cell = new CellImpl(ROW, COL, CellType.EXIT);
+        assertTrue(cell.isWalkable(), "the EXIT cells should be walkable");
+    }
+}
